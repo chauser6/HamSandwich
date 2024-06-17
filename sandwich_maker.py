e09858ddf4fddbda030fd1520369034af4dae31e
chauser6 <carlena.hauser1108@gmail.com>
2024-06-16 22:29:48 -0400

    feat(sandwich_maker): add methods to check resources and make sandwiches
    - Added check_resources method to validate ingredient availability
    - Added make_sandwich method to deduct ingredients and confirm sandwich making

--- sandwich_maker.py
+++ sandwich_maker.py
@@ -4,7 +4,12 @@ class SandwichMaker:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        for item in ingredients:
+            if ingredients[item] > self.machine_resources.get(item, 0):
+                return False
+        return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        for item in order_ingredients:
+            self.machine_resources[item] -= order_ingredients[item]
+        print(f"Making your {sandwich_size} sandwich!")
