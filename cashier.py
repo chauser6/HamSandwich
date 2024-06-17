3be820e2ae1ec75ec7f8242b291b514e2aa90245
chauser6 <carlena.hauser1108@gmail.com>
2024-06-16 22:31:47 -0400

    feat(cashier): add methods to process coins and handle transactions
    - Added process_coins method to calculate total from inserted coins
    - Added transaction_result method to validate payment and provide change or refund

--- cashier.py
+++ cashier.py
@@ -5,9 +5,20 @@ class Cashier:
     def process_coins(self):
         """Returns the total calculated from coins inserted.
            Hint: include input() function here, e.g. input("how many quarters?: ")"""
-        ###
+        print("Please insert coins.")
+        total = int(input("How many quarters?: ")) * 0.25
+        total += int(input("How many dimes?: ")) * 0.10
+        total += int(input("How many nickels?: ")) * 0.05
+        total += int(input("How many pennies?: ")) * 0.01
+        return total
 
     def transaction_result(self, coins, cost):
         """Return True when the payment is accepted, or False if money is insufficient.
            Hint: use the output of process_coins() function for cost input"""
-        ##
+        if coins >= cost:
+            change = round(coins - cost, 2)
+            print(f"Here is ${change} in change.")
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            return False
